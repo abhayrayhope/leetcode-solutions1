98e3e42e4ee73d0004c29c66c9236b48a96952e4
abhayrayhope <abhaykumar692006@gmail.com>
2026-08-07 01:37:42 +0530

    Time: 1254 ms (9.8%), Space: 37.2 MB (6.56%) - LeetHub

--- 4010-maximize-pair-strength-using-gcd/4010-maximize-pair-strength-using-gcd.cpp
+++ 4010-maximize-pair-strength-using-gcd/4010-maximize-pair-strength-using-gcd.cpp
@@ -5,7 +5,7 @@ public:
 long long m=0;
         long long x=0;
          for(int i=0;i<nums.size();i++)
-         { for(int j=0;j<nums.size();j++){
+         { for(int j=i+1;j<nums.size();j++){
             if(i!=j){
              x=((1LL*nums[i] * nums[j]) )/ ((1LL*gcd(nums[i], nums[j]))* (1LL*gcd(nums[i], nums[j])));}
 

746dbe20a381d96aa8cae54d8dc7578f1b98da5e
abhayrayhope <abhaykumar692006@gmail.com>
2026-08-22 17:23:08 +0530

    Time: 0 ms (100%), Space: 8.5 MB (68.16%) - LeetHub

---
+++ 3798-largest-even-number/3798-largest-even-number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string largestEven(string s) {
+
+int i=s.size()-1;
+
+while(i>=0)
+{
+   if(s[i]=='2')
+   {
+    return  s.substr(0, i + 1);
+   }
+i--;
+
+}
+
+    return "";    
+    }
+};
